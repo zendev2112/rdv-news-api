ba2a97c2282978e04186a4019e6f980c3fc5b58c
zen <zenon.deviagge@gmail.com>
2025-04-23 19:27:51 -0300

    server keeps on failing. Syntax error, part 3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start"
+  }
 }
 
 module.exports = nextConfig
