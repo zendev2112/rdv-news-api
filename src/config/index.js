29534b77783e39478348ee440fcae8e2a711a305
zen <zenon.deviagge@gmail.com>
2025-10-21 17:18:36 -0300

    everything working except cerebras

--- src/config/index.js
+++ src/config/index.js
@@ -129,7 +129,7 @@ const config = {
 
   cerebras: {
     apiKey: process.env.CEREBRAS_API_KEY || '',
-    model: process.env.CEREBRAS_MODEL || 'cpt-7b',
+    model: process.env.CEREBRAS_MODEL || 'llama-3.3-70b',
   },
   openrouter: {
     apiKey: process.env.OPENROUTER_API_KEY || '',
